1f7b62ea41d93e605bcf181790e9a22e46900a5f
Thomas Knutsen <knutsenthomas@gmail.com>
2026-02-27 08:34:07 +0100

    Fix build error: Rename entry.js to entry.jsx

--- src/app/entry.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Root from '@/app/Root';
-import '@/styles/index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Root />
-);
